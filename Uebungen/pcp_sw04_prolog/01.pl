6949cf0782da4ae1599384691571319b99cf494d
PatrickRichner <patrick.richner@stud.hslu.ch>
2020-04-05 21:29:31 +0200

    correction explanation

--- Uebungen/pcp_sw04_prolog/01.pl
+++ Uebungen/pcp_sw04_prolog/01.pl
@@ -6,7 +6,8 @@ fib_clp(N, F) :- { N >= 2, F2 >= 0, F1 >= 1, F >= 1, F >= N - 1, F1 >= F2, F = F
     fib_clp(N1, F1),
     fib_clp(N2, F2).
 
-% a.) Es wird ohne ende einfach weiter nach einer Lösung gesucht es gibt viel zu viele Möglichkeiten.
+% a.) Es wird ohne ende einfach weiter nach einer Lösung gesucht. Es gibt viel zu viele Möglichkeiten.
+
 % b.) Die reelen Zahlen sind nicht endlich, und es wird nach einer weiteren Lösung gesucht, obwohl es keine gibt oder es einfach so viele möglichkeiten gibt..
 %      Es gibt hier auch keine "Abbruchbedingung bei der suche nach möglichen F1 & F2. Auch negative und gleitkommazahlen sind möglich.
 % c.) clpq modus, nur ganzzahlen, reduziert die anzahl möglichkeiten massiv.
