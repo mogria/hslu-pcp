e7d61e24d9f1f3e5a35f5b48cf989df79c370bf9
PatrickRichner <patrick.richner@stud.hslu.ch>
2020-05-05 13:11:42 +0200

    Add parts sw05 scheme

--- Uebungen/pcp_sw05_scheme/03.rkt
+++ Uebungen/pcp_sw05_scheme/03.rkt
@@ -1,3 +1,6 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "advanced-reader.rkt" "deinprogramm" "sdp")((modname |03|) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none datum #f ())))
 ;;; 3.) Definieren Sie Länge und Breite eines Rechtecks und berechnen Sie dessen Diagonale.
 (define (square x) (* x x))
 (define (diagonal width height) 
